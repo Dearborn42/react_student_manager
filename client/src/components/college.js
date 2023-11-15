cf3cecdabad970474c1f64042e8f6acc29978d5a
Dearborn42 <amurf26@outlook.com>
2023-11-15 15:23:16 -0700

    adding css and bootstrap

--- client/src/components/college.js
+++ client/src/components/college.js
@@ -1,13 +1,12 @@
 import React from "react";
 import StudentList from "./studentList";
 import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 export default function College({data}){
     const [list, updateStudentList] = useState(data);
-    const [form, setForm] = useState({
-        name: "",
-        age: "",
-        course: "",
-    });
+    const [form, setForm] = useState({name: "", age: "", course: ""});
+    const [type, updateType] = useState(true);
     function updateForm(value) {
         return setForm((prev) => {
             return { ...prev, ...value };
@@ -16,37 +15,56 @@ export default function College({data}){
     function updateStudents(e){
         e.preventDefault();
         const studentForm = {...form}
-        updateStudentList((prevList) => [...prevList, studentForm]);
+        if(type)
+            updateStudentList((prevList) => [studentForm, ...prevList]);
+        else
+            updateStudentList((prevList) =>
+                prevList.filter((student) => student.name !== studentForm.name && student.age !== studentForm.age && student.course !== studentForm.course)
+            );
         updateForm({name: "",age: "",course: "",})
+    }
+    function addDelStudent(){
+        updateType(!type)
     }
     return(
         <>
-        <form onSubmit={updateStudents}>
-            <input
-                type="text"
-                className="form-control"
-                id="name"
-                value={form.name}
-                onChange={(e) => updateForm({ name: e.target.value })}
-            />
-            <br />
-            <input
-                type="text"
-                className="form-control"
-                id="age"
-                value={form.age}
-                onChange={(e) => updateForm({ age: e.target.value })}
-            />
-            <br />
-            <input
-                type="text"
-                className="form-control"
-                id="course"
-                value={form.course}
-                onChange={(e) => updateForm({ course: e.target.value })}
-            />
-            <button type="submit">Add student</button>
-        </form>
+        <div className="mx-auto w-25 h-50 d-flex flex-column">
+            <Button variant="primary" onClick={addDelStudent}  className="mt-1">Change form function</Button>
+            <form onSubmit={updateStudents}>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    value={form.name}
+                    onChange={(e) => updateForm({ name: e.target.value })}
+                />
+                <br />
+                <input
+                    type="text"
+                    className="form-control"
+                    id="age"
+                    value={form.age}
+                    onChange={(e) => updateForm({ age: e.target.value })}
+                />
+                <br />
+                <input
+                    type="text"
+                    className="form-control"
+                    id="course"
+                    value={form.course}
+                    onChange={(e) => updateForm({ course: e.target.value })}
+                />
+                <div className="d-flex justify-content-center w-100">
+                    <Button 
+                        variant="primary" 
+                        onClick={addDelStudent} 
+                        type="submit">
+                            {type === true ? ("Add") : ("Remove")} student
+                    </Button>
+                </div>
+                
+            </form>
+        </div>
         <StudentList studentsList={list} />
         </>
     );
--- client/src/components/studentList.js
+++ client/src/components/studentList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Student from './studentDisplay';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function StudentList({studentsList}){
     return (
-        studentsList.map(x => (<Student student={x}/>))
+        <div className="d-flex flex-wrap justify-content-evenly">
+            {studentsList.map(x => (<Student student={x}/>))}
+        </div>
     )
 }
